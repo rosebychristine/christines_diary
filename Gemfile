276ae3d532214e9529979ee90042b4327f49fb6e
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-04-08 21:11:57 +0900

    Release to heroku

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'sqlite3' 
 end
 
 group :development do
@@ -54,3 +55,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem "haml-rails"
 gem 'font-awesome-sass'
+
+group :production do
+  gem 'pg'
+end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,15 @@
 class ContactMailer < ApplicationMailer
-
+  def contact_mail(contact)
+    @contact = contact  
+    mail to: ENV['MAIL'], subject: "メールのタイトル"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.contact_mailer.contact_mail.subject
   #
-  def contact_mail
-    @greeting = "Hi"
+  # def contact_mail
+  #   @greeting = "Hi"
 
-    mail to: "to@example.org"
+  #   mail to: "to@example.org"
   end
 end
