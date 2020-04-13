5425791b31ec67492a0200533a66a7b31f03e809
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-04-13 20:48:47 +0900

    fix bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'アプリ名.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'christine-hata.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
