402690763738ac81597c4ab1be0a78591090d355
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-04-25 17:02:57 +0900

    fix

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,25 @@
+// function toggleNav() {
+//   var body = document.body;
+//   var hamburger = document.getElementById('js-hamburger');
+//   var blackBg = document.getElementById('js-black-bg');
+
+//   hamburger.addEventListener('click', function() {
+//     body.classList.toggle('nav-open');
+//   });
+//   blackBg.addEventListener('click', function() {
+//     body.classList.remove('nav-open');
+//   });
+// }
+// toggleNav();
+
+
+// $(document).ready(function() {
+//   $(".menu-trigger").click(function () {
+//     $(this).toggleClass("active");
+//     $(this).next().toggleClass("onanimation");
+//     $('ul li').hide();
+//     $('ul li').each(function(i) {
+//   $(this).delay(80 * i).fadeIn(500);
+//     });
+//   });
+//  });
